0348bb7fe941188eab40963ae4d3c12a54ec49cf
nictuss <nicolo.palmiero@gmail.com>
2020-05-22 22:32:50 +0200

    started to code the front-end to display suggestions

--- src/public_dashboard/index.js
+++ src/public_dashboard/index.js
@@ -1,11 +1,6 @@
-/*
-THIS IS THE SERVER SIDE SCRIPT
-*/
-
 const express = require('express');
 const socket = require('socket.io');
 let bodyParser = require('body-parser');
-let DBHandler = require('./scripts/dbhandler');
 
 //Exprees allows you to handle the place where to find static resources, and allows you to create a server
 const app = express();
@@ -24,17 +19,16 @@ console.log(`Listening to requests on http://localhost:${port}`);
 //This is the middleware to enable communication between server and clients(web pages)
 let io = socket(server);
 
-//This is the handler for the db, you will find this class in another file
-let db = new DBHandler();
-
 io.on('connection', function(socket){
 
     //The server is now connected with the client and you can start performing operations
-
+    console.log('SERVER: connection made');
     //This is express that allows you to handle http post requests (for now the dashboard will receive http post messages)
     app.post('/',function(request,response){
-        //request is the variable in which the message the dashboard received is stored
-        //You have to send the message to the client to display it, so you will write something like this:
-        io.emit('<label attached to the message>', request);
+        socket.emit('suggestions', request);
     });
+
+    setInterval(function () {
+        socket.emit('suggestions', ['room1', 'room2', 'room3']);
+    }, 10000);
 });
--- src/public_dashboard/public/js/dataDisplayer.js
+++ src/public_dashboard/public/js/dataDisplayer.js
@@ -1,7 +1,45 @@
-//make connection to the backend, this variable is enabled by the html file
 var socket = io({transports: ['websocket']});
 
+console.log('CLIENT: connection made');
+
+//keep the last suggestions
+let last_suggestions = [];
+let button = null;
+
+//function that displays the suggestion it takes in input
+function display_suggestion(suggestion){
+  console.log(suggestion);
+}
+
+//function associated to the button in the webapp to display the next suggestion
+function next_suggestion(){
+  let suggestion = last_suggestions.pop();
+  if(suggestion){
+    display_suggestion(suggestion);
+  }
+  else{
+    alert('there are no more suggestions available for you :(');
+  }
+}
+
+//Function to create button if it does not exist
+function create_next_suggestion_button(){
+  if(button == null){
+    button = document.createElement('button');
+    button.innerText = 'Next Room';
+    const body = document.getElementById('body');
+    button.addEventListener("click", event => {
+      next_suggestion();
+    });
+    body.appendChild(button);
+  }
+}
+
+
 //You can create multiple instances of these listeners to listen for different kind of messages
-socket.on('The same label you assigned to the message in the server side script>', function(msg){
-  //Do something to display data
+socket.on('suggestions', function(suggestions){
+  alert('You have a new suggestion!');
+  last_suggestions = suggestions.reverse();
+  next_suggestion();
+  create_next_suggestion_button();
 });
